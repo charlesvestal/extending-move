4ec5e9c9554b6a82bc37657d22c1ef6eb4837529
Charles Vestal <charles@charles.pizza>
2025-06-06 10:15:00 +0200

    Fix pad grid html assembly

--- handlers/set_management_handler_class.py
+++ handlers/set_management_handler_class.py
@@ -206,9 +206,13 @@ class SetManagementHandler(BaseHandler):
                 disabled = 'disabled' if occupied else ''
                 color_id = color_map.get(idx)
                 style = f' style="background-color: {rgb_string(color_id)}"' if color_id else ''
-                input_html = f'<input type="radio" id="pad_{num}" name="pad_index" value="{num}" {disabled}>'
+                input_html = (
+                    f'<input type="radio" id="pad_{num}" name="pad_index" '
+                    f'value="{num}" {disabled}>'
+                )
                 label_html = (
-                    f'<label for="pad_{num}" class="pad-cell {status}"{style}>{label_text}</label>'
+                    f'<label for="pad_{num}" class="pad-cell {status}"{style}>'
+                    f'{label_text}</label>'
                 cell_html = input_html + label_html
             options.append(option)
                     f'<label for="pad_{num}" class="pad-cell {status}"{style}>{label_text}</label>'
